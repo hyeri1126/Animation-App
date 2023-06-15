feee42c2c0ccea13ac392a4669ac0d54da217c68
hyeri1126 <hyeri1126@ewhain.net>
2023-06-15 21:37:50 +0900

    View 컴포넌트에 animation 효과 적용하기

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { Animated } from 'react-native';
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex:1;
+  justify-content: center;
+  align-items: center;
+`
+const Box = styled.View`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
+  const Y = new Animated.Value(0)
+  const moveUp = () => {
+    Animated.spring(Y, {
+      toValue:-200,
+      bounciness:30,
+      useNativeDriver:true,
+    }).start();
+  };
+  console.log(Y)
+  Y.addListener(()=>console.log(Y))
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <TouchableOpacity onPress={moveUp} >
+        <AnimatedBox
+          style={{
+            transform : [{translateY:Y}]
+          }}>
+            
+        </AnimatedBox>
+      </TouchableOpacity>
+     
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
