3cdac21c179674b21b6176c485ffda01b4783fdf
hyeri1126 <hyeri1126@ewhain.net>
2023-06-29 18:03:09 +0900

    두장의 카드로 무한히 많은 카드처럼 보이는 애니메이션 효과 (카드마다 아이콘 바뀜)

--- App.js
+++ App.js
@@ -1,80 +1,159 @@
 import React, { useRef, useState } from 'react';
-import { Dimensions, PanResponder, Pressable, TouchableOpacity , Animated, ProgressBarAndroidBase} from 'react-native';
+import { Dimensions, Pressable, TouchableOpacity , Animated, ProgressBarAndroidBase, View, PanResponder, Text} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import icons from './icons';
+
 import styled from 'styled-components/native';
 
 const Container = styled.View`
   flex:1;
   justify-content: center;
   align-items: center;
+  background-color: blue;
 `
-const Box = styled.View`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
+const Card = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  width: 250px;
+  height: 250px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  box-shadow: 1px 1px 5px rgba(1, 1, 1 ,0.3);
+  position: absolute;
 `
-const AnimatedBox = Animated.createAnimatedComponent(Box)
-
-
+const Btn = styled.TouchableOpacity`
+  margin: 0 5px;
+`
+const BtnContaienr = styled.View`
+  flex-direction: row;
+  flex: 1;
+  margin-top: -100px;
+`
+const CardContainter = styled.View`
+  flex:3;
+  justify-content: center;
+  align-items: center;
+`
 export default function App() {
-  const POSITION = useRef(
-    new Animated.ValueXY({
-      x:0, 
-      y:0,
+  
+  // Values
+  const scale = useRef(new Animated.Value(1)).current;
+  const position = useRef(new Animated.Value(0)).current;
+  // InterPolates
+  const rotation = position.interpolate({
+    inputRange: [-250, 250],
+    outputRange: ["-15deg", "15deg"],
+    //extrapolate에서는 inputRange 바깥으로 나갔을 때 어떻게 처리할지 명시할 수 있음
+  });
+  const secondScale = position.interpolate({
+    inputRange:[-280, 0, 280],
+    outputRange:[1, 0.5, 1],
+    extrapolate:"clamp",
+  })
+  //Animations
+  //Animated.spring은 애니메이션이 완료되는데에 시간이 좀 걸림. bounce 효과 때문에! 
+  //Animated.spring을 빠르게 만드는 옵션 두가지 : restSpeedThreshold, restDisplacementThreshold 
+  // -> 둘을 조합해서 사용하면 애니메이션을 빨리 끝낼 수 있음. 
+  const onPressIn = Animated.spring(scale, {
+      toValue:0.95, 
+      useNativeDriver:true,
+  });
+  const onPressOut = Animated.spring(scale,{
+    toValue:1, 
+    useNativeDriver:true,
+  });
+  const goCenter = Animated.spring(position, {
+    toValue:0,
+    useNativeDriver:true,
+  });
+  const goLeft = Animated.spring(position, {
+    toValue:-500,
+    tension: 1,
+    useNativeDriver:true,
+    restSpeedThreshold:100,
+    restDisplacementThreshold:100,
+  });
+  const goRight = Animated.spring(position,{
+    toValue:500,
+    tension:1,
+    useNativeDriver:true,
+    restSpeedThreshold:100,
+    restDisplacementThreshold:100,
+  });
+  // State 
+  const [index, setIndex] = useState(0);
+  // 카드가 옆으로 사라질 때 index 1 올려주는 함수
+  const onDismiss = () => {
+    scale.setValue(1);
+    position.setValue(0);
+    setIndex((prev) => prev + 1);
+  }
+  const closePress = () => {
+    goLeft.start(onDismiss);
+  }
+  const checkPress = () => {
+    goRight.start(onDismiss);
+  }
+  
+  //Pan Responders
+  const panResponder = useRef(
+    PanResponder.create({
+      //터치에 의해 동작하겠다는 의미
+      onStartShouldSetPanResponder : () => true,
+      //panResponder 내에서 사용자가 터치하기 시작하면 호출되는 두가지 함수가 있음. -> onPanRespond,erGrant,onPanResponderRelease
+      onPanResponderMove: (_, {dx}) => {
+        position.setValue(dx);
+      },
+      onPanResponderGrant: () => onPressIn.start(),
+      onPanResponderRelease: (_,{dx}) => {
+        if(dx < -230){
+          goLeft.start(onDismiss);
+        } else if(dx > 230) {
+          goRight.start(onDismiss);
+        } else {
+          Animated.parallel([onPressOut,goCenter]).start();
+        }
+        
+      },
     })
   ).current;
   
-  const borderRadius = POSITION.y.interpolate({
-    inputRange: [-200, 200 ],
-    outputRange: [100, 0 ]
-  })
-  const bgColor = POSITION.y.interpolate({
-    inputRange:[-200,200],
-    outputRange:["rgb(255,99,71)", "rgb(71,166,255)"]
-  })
-  const panResponder = useRef(
-    PanResponder.create({
-      onStartShouldSetPanResponder:() => true,
-      //터치가 시작될 때 호출되는 함수.
-      onPanResponderGrant: () => {
-        console.log("Touch Started");
-        POSITION.setOffset({
-          //전에 있었던 위치의 값을 받아옴.
-          x: POSITION.x._value,
-          y: POSITION.y._value,
-        })
-      },
-      onPanResponderMove:(_, {dx,dy}) => {
-        console.log("Finger Moving")
-        //dx,dy는 터치한 시작부터 움직인 거리임. 항상 0부터 시작. 
-        POSITION.setValue({
-          x:dx,
-          y:dy,
-        });
-      },
-      onPanResponderRelease: () => {
-        console.log("Touch Finished");
-        POSITION.flattenOffset();
-      },
-    })
-  ).current;
-  console.log(PanResponder);
   return (
-      <TouchableOpacity 
-        style={{flex:1, alignItems:'center', justifyContent:'center'}} 
+      <Container>
+        <CardContainter >
+          <Card 
+            style={{
+              transform:[
+                {scale:secondScale}
+              ]
+            }}>
+              <Text>Back Card</Text>
+            <Ionicons name={icons[index+1]} size={98} color="#192a56" />
+          </Card>
+          <Card 
+            {...panResponder.panHandlers}
+            style={{
+              transform:[
+                {scale}, 
+                {translateX:position}, 
+                {rotateZ:rotation},
+              ]
+            }}>
+              <Text>Front Card</Text>
+            <Ionicons name={icons[index]} size={98} color="#192a56" />
+          </Card>
+        </CardContainter>
+        
+        <BtnContaienr>
+          <Btn onPress={closePress}>
+            <Ionicons name='close-circle' color="white" size={42}  />
+          </Btn>
+          <Btn onPress={checkPress}>
+            <Ionicons name='checkmark-circle' color="white" size={42}  />
+          </Btn>
+        </BtnContaienr>
        
-      >
-        <AnimatedBox
-        {...panResponder.panHandlers}
-          style={{
-            borderRadius,
-            backgroundColor:bgColor,
-            transform : [
-              ...POSITION.getTranslateTransform(),
-            ],
-          }}>
-            
-        </AnimatedBox>
-      </TouchableOpacity>
+      </Container>
      
   );
 }
